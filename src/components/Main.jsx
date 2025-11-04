84ed6982d90fbc11cc0846a0f5c7ee97a368138d
LucaStellato <lucastellato435@gmail.com>
2025-11-04 14:43:48 +0100

    add final icon with description

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,8 @@
 import comics from '../assets/img/buy-comics-digital-comics.png';
 import merchandise from '../assets/img/buy-comics-merchandise.png';
+import subscription from '../assets/img/buy-comics-subscriptions.png';
+import locator from '../assets/img/buy-comics-shop-locator.png';
+import power from '../assets/img/buy-dc-power-visa.svg';
 
 
 export default function Main() {
@@ -14,8 +17,12 @@ export default function Main() {
                 <span className='mt-3 mx-3'>DIGITAL COMICS</span>
                 <img src={merchandise} style={{ height: '60px' }} />
                 <span className='mt-3 mx-3'>DC MERCHANDISE</span>
-                <img src={comics} style={{ height: '40px' }} />
-                <img src={comics} style={{ height: '40px' }} />
+                <img src={subscription} style={{ height: '60px' }} />
+                <span className='mt-3 mx-3'>SUBSCRIPTIONS</span>
+                <img src={locator} style={{ height: '60px' }} />
+                <span className='mt-3 mx-3'>COMIC SHOP LOCATOR</span>
+                <img src={power} style={{ height: '60px' }} />
+                <span className='mt-3 mx-3'>CDC POWER VISA</span>
             </div>
 
         </>
