47050f92e400ccd1e9fb4049ac7faec0bc78c923
LucaStellato <lucastellato435@gmail.com>
2025-11-04 14:59:38 +0100

    center the image with description

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,21 +8,21 @@ import power from '../assets/img/buy-dc-power-visa.svg';
 export default function Main() {
     return (
         <>
-            <div className=" p-4" style={{ backgroundColor: 'black', minHeight: '120px', color: 'white', fontSize: '20px' }}>
+            <div className=" p-4 " style={{ backgroundColor: 'black', minHeight: '120px', color: 'white', fontSize: '20px' }}>
                 <span style={{ marginTop: '20px', marginLeft: '60px' }}>--Content goes here--</span>
 
             </div >
-            <div className="d-flex justify-content-center" style={{ backgroundColor: 'blue', minHeight: '200px', color: 'white' }}>
-                <img src={comics} style={{ height: '60px' }} />
-                <span className='mt-3 mx-3'>DIGITAL COMICS</span>
-                <img src={merchandise} style={{ height: '60px' }} />
-                <span className='mt-3 mx-3'>DC MERCHANDISE</span>
-                <img src={subscription} style={{ height: '60px' }} />
-                <span className='mt-3 mx-3'>SUBSCRIPTIONS</span>
-                <img src={locator} style={{ height: '60px' }} />
-                <span className='mt-3 mx-3'>COMIC SHOP LOCATOR</span>
-                <img src={power} style={{ height: '60px' }} />
-                <span className='mt-3 mx-3'>CDC POWER VISA</span>
+            <div className="d-flex justify-content-center " style={{ backgroundColor: 'blue', minHeight: '200px', color: 'white' }}>
+                <img src={comics} style={{ height: '60px', marginTop: '50px' }} />
+                <span className=' mx-3' style={{ marginTop: '60px', paddingRight: '30px' }}>DIGITAL COMICS</span>
+                <img src={merchandise} style={{ height: '60px', marginTop: '50px' }} />
+                <span className=' mx-3' style={{ marginTop: '60px', paddingRight: '30px' }}>DC MERCHANDISE</span>
+                <img src={subscription} style={{ height: '60px', marginTop: '50px' }} />
+                <span className=' mx-3' style={{ marginTop: '60px', paddingRight: '30px' }}>SUBSCRIPTIONS</span>
+                <img src={locator} style={{ height: '60px', marginTop: '50px' }} />
+                <span className=' mx-3' style={{ marginTop: '60px', paddingRight: '30px', paddingRight: '30px' }}>COMIC SHOP LOCATOR</span>
+                <img src={power} style={{ height: '60px', marginTop: '50px' }} />
+                <span className=' mx-3' style={{ marginTop: '60px', paddingRight: '30px' }}>CDC POWER VISA</span>
             </div>
 
         </>
