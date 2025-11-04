b5d15f9da18aa443827951968f062e0730e30849
LucaStellato <lucastellato435@gmail.com>
2025-11-04 14:20:15 +0100

    add color seond part body

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,14 @@
 export default function Main() {
     return (
-        <div className=" p-4" style={{ backgroundColor: 'black', minHeight: '120px', color: 'white', fontSize: '20px', }}>
-            <span style={{ marginTop: '20px', marginLeft: '60px' }}>--Content goes here--</span>
-        </div >
+        <>
+            <div className=" p-4" style={{ backgroundColor: 'black', minHeight: '120px', color: 'white', fontSize: '20px' }}>
+                <span style={{ marginTop: '20px', marginLeft: '60px' }}>--Content goes here--</span>
+
+            </div >
+            <div style={{ backgroundColor: 'blue', minHeight: '180px' }}>
+
+            </div>
+
+        </>
     )
 }
