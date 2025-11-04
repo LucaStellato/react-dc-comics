03b38ac8be71c25eb545af8f05a074dc26481ef8
LucaStellato <lucastellato435@gmail.com>
2025-11-04 14:34:11 +0100

    insert first image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import cell from '../assets/img/buy-comics-digital-comics.png';
+import comics from '../assets/img/buy-comics-digital-comics.png';
 
 export default function Main() {
     return (
@@ -7,8 +7,12 @@ export default function Main() {
                 <span style={{ marginTop: '20px', marginLeft: '60px' }}>--Content goes here--</span>
 
             </div >
-            <div style={{ backgroundColor: 'blue', minHeight: '200px' }}>
-
+            <div className="d-flex justify-content-center" style={{ backgroundColor: 'blue', minHeight: '200px', color: 'white' }}>
+                <img src={comics} style={{ height: '60px' }} />
+                <span className='mt-3 mx-3'>DIGITAL COMICS</span>
+                <img src={comics} style={{ height: '40px' }} />
+                <img src={comics} style={{ height: '40px' }} />
+                <img src={comics} style={{ height: '40px' }} />
             </div>
 
         </>
